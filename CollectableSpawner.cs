5cbdfec10d0634e73f729258859c628c4a7b484b
Demonox2 <35931572+Demonox2@users.noreply.github.com>
2018-12-14 21:51:27 -0500

    Add files via upload
    Updating with Spawners. Rudimentary AI Director logic in Enemy/Collectable Spawners as well. Not working as intended currently.

---
+++ CollectableSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+
+    public GameObject Collectable;
+
+    public float DownTime = 10.0f;
+
+    private float SpawnTimer = 5.0f;
+
+    private PlayerController playercontroller;
+
+    
+
+
+    private void Awake()
+    {
+        playercontroller = GetComponent<PlayerController>(); //to reference PlayerController health values
+        
+    }
+
+    /*private float NewHealth = (playercontroller.Maxhealth - playercontroller.health)/10;*/
+
+    void Update()
+    {
+        if (Time.time >= SpawnTimer)
+        
+        {
+          SpawnCollectables();
+          SpawnTimer = Time.time + DownTime /*NewHealth*/;
+        }
+
+    }
+
+    public void SpawnCollectables()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex == i)
+            {
+                Instantiate(Collectable, spawnPoints[i].position, spawnPoints[i].rotation);
+            }
+        }
+    }
+}
---
+++ EnemySpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+
+    public GameObject Enemy;
+
+    public float DownTime = 10.0f;
+
+    private float SpawnTimer = 5.0f;
+
+    private PlayerController playercontroller;
+
+    private void Awake()
+    {
+        playercontroller = GetComponent<PlayerController>();//to reference PlayerController health values
+    }
+
+    void Update()
+    {
+        if (Time.time >= SpawnTimer)
+
+        {
+            SpawnCollectables();
+            SpawnTimer = Time.time + DownTime;
+        }
+
+    }
+
+    public void SpawnCollectables()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex == i)
+            {
+                Instantiate(Enemy, spawnPoints[i].position, spawnPoints[i].rotation);
+            }
+        }
+    }
+}
